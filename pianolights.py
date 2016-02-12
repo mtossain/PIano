3298df6c3a875dbce515560114b5ea7e585e734a
Michel Tossaint <mmmtossaint@gmail.com>
2016-02-12 10:22:34 +0100

    Update pianolights.py

--- pianolights.py
+++ pianolights.py
@@ -24,54 +24,48 @@ GPIO.setmode(GPIO.BCM)
 # CONFIG
 #########################################
 
-candleleftpin = 8                       # Pin for left candle LED
-candlerightpin = 7                      # Pin for left candle LED
-starpin = 6                             # Pin for starlights
-pirpin = 10                             # Pin for PIR motion detect sensor
+PinCandleLeft = 8           # Pin for left candle LED
+PinCandleRight = 7          # Pin for left candle LED
+PinStarLights = 6           # Pin for starlights
+PinMotionDetect = 15        # Pin for PIR motion detect sensor
 
-GPIO.setup(candleleftpin, GPIO.OUT)
-GPIO.setup(candlerightpin, GPIO.OUT)
-GPIO.setup(starpin, GPIO.OUT)
-GPIO.setup(pirpin, GPIO.IN)
+TimeOutLights = 5           # After x seconds lights turn off again
 
-GPIO.output(starpin,GPIO.LOW)    
-GPIO.output(candleleftpin,GPIO.LOW)
-GPIO.output(candlerightpin,GPIO.LOW)
+GPIO.setup(PinCandleLeft, GPIO.OUT)
+GPIO.setup(PinCandleRight, GPIO.OUT)
+GPIO.setup(PinStarLights, GPIO.OUT)
+GPIO.setup(PinMotionDetect, GPIO.IN)
 
-motiondetected = datetime.datetime(1,1,1)
+GPIO.output(PinStarLights,GPIO.LOW)    
+GPIO.output(PinCandleLeft,GPIO.LOW)
+GPIO.output(PinCandleRight,GPIO.LOW)
+
+motiondetected = datetime.datetime(1,1,1) # Initialise for first use
 
 #########################################
 # FUNCTIONS
 #########################################
 
-def MOTION(PIR_PIN):
+def Motion():
     motiondetected = datetime.datetime.now()
 
 def CandleCtrl():
-	
 	while True:
-		
 		timesincemotion = datetime.datetime.now() - motiondetected
-
-		if timesincemotion.total_seconds() < 5*60: # after x seconds switch off candles
-			GPIO.output(candleleftpin,GPIO.HIGH) # Put the left LED on
-			GPIO.output(candlerightpin,GPIO.HIGH) # Put the right LED on
-			GPIO.output(starpin,GPIO.HIGH)
-
-		else:
-			GPIO.output(candleleftpin,GPIO.LOW) # Put the left LED off
-			GPIO.output(candlerightpin,GPIO.LOW) # Put the right LED off
-			GPIO.output(starpin,GPIO.LOW)
-		
+                if timesincemotion.total_seconds() < TimeOutLights:
+			GPIO.output(PinCandleLeft,GPIO.HIGH)
+			GPIO.output(PinCandleRight,GPIO.HIGH)
+			GPIO.output(PinStarLights,GPIO.HIGH)
+	        else:
+		        GPIO.output(PinCandleLeft,GPIO.LOW) 
+		        GPIO.output(PinCandleRight,GPIO.LOW) 
+		        GPIO.output(PinStarLights,GPIO.LOW)
 		time.sleep(1)
-
 
 MidiThread = threading.Thread(target=CandleCtrl)
 MidiThread.daemon = True
 MidiThread.start()
 
-# Whenever the pin rises the interrupt is called
-GPIO.add_event_detect(pirpin, GPIO.RISING, callback=MOTION)
-# do not close the program, but keep on looking for the interrupt
-while 1:
+GPIO.add_event_detect(PinMotionDetect, GPIO.RISING, callback=Motion()) # Whenever the pin rises the interrupt is called
+while 1: # do not close the program, but keep on looking for the interrupt
     time.sleep(100)
